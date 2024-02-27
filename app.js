06481dc4a5688e9ad6b3f313a4920c5683ae89bd
vvptitsyna <81580544+vvptitsyna@users.noreply.github.com>
2024-02-27 05:47:13 +0300

    Update app.js
    fix name

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
-const taskRoutes = require('./routes/tasks');
+const taskRoutes = require('./routes/routes');
 
 const app = express();
 
